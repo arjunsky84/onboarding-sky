862841e8a0d908edd4ef22271c3cb04ceb788547
Arjun Trivedi <atd04@ukgc2kn69hk9.bskyb.com>
2023-07-13 15:42:25 +0100

    clean up

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-async function getProducts() {
-    const response = await fetch("https://products-api-kappa.vercel.app/api/products");
-    const products = await response.json();
-    return products
-}
-
-export { getProducts }
